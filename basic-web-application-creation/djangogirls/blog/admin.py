3a57e5f01b07273b175131bdd8a723d9c9566670
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-03-14 19:54:00 +0300

    Post model is registered to make it visible on the admin page.

--- basic-web-application-creation/djangogirls/blog/admin.py
+++ basic-web-application-creation/djangogirls/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# To make our model visible on the admin page, we need to register the model with the following line:
+admin.site.register(Post)
+
