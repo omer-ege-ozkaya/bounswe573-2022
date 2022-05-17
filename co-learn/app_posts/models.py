1960087ed884a056a2866981091f1213455aad89
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-17 20:04:58 +0300

    Modify post model, this is in progress. Take checkpoint before recreating migration files of app_post.

--- co-learn/app_posts/models.py
+++ co-learn/app_posts/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
 
 
 class Post(models.Model):
-    text = models.TextField()
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
+    category = models.TextField()
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.text[:50]
+        return self.title[:50]
+
