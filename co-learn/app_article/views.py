42841ea6558a71b1b91d25a270bfdfafdfc68e55
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-27 18:35:08 +0300

    Add update article.

--- co-learn/app_article/views.py
+++ co-learn/app_article/views.py
@@ -42,24 +42,24 @@ def create_article_view(req, learning_space_id):
     return HttpResponse(template.render(context, req))
 
 
-# def update_article_view(req, article_id):
-#     article_model = LearningSpace.objects.get(id=article_id)
-#     if req.method == "POST":
-#         form = Article(req.POST, instance=article_model)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(f"/learning-space/{article_model.id}")
-#     else:
-#         article_as_dict = model_to_dict(article_model)
-#         form = Article(initial=article_as_dict)
-#
-#     template = loader.get_template("app_article/article_update.html")
-#     context = {
-#         "form": form
-#     }
-#     return HttpResponse(template.render(context, req))
-#
-#
+def update_article_view(req, article_id):
+    article_model = Article.objects.get(id=article_id)
+    if req.method == "POST":
+        form = ArticleForm(req.POST, instance=article_model)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(f"/article/{article_model.id}")
+    else:
+        article_as_dict = model_to_dict(article_model)
+        form = ArticleForm(initial=article_as_dict)
+
+    template = loader.get_template("app_article/article_update.html")
+    context = {
+        "form": form
+    }
+    return HttpResponse(template.render(context, req))
+
+
 # def delete_article_view(req, article_id):
 #     article = LearningSpace.objects.get(id=article_id)
 #     article.delete()
