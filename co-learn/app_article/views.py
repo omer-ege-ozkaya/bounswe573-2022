0e6f424e68628cd0ee3dd37f2414dba3b9497670
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-27 20:34:41 +0300

    Post form.

--- co-learn/app_article/views.py
+++ co-learn/app_article/views.py
@@ -6,15 +6,18 @@ from app_posts.models import Post
 from app_profile.models import Profile
 from .forms import ArticleForm
 from django.forms.models import model_to_dict
+from app_posts.forms import PostForm
 
 
 def article_view(req, article_id):
     template = loader.get_template("app_article/article.html")
     article = Article.objects.get(id=article_id)
     posts = Post.objects.filter(article__id=article_id).order_by("created_at")
+    post_form = PostForm()
     context = {
         "article": article,
-        "posts": posts
+        "posts": posts,
+        "post_form": post_form
     }
     return HttpResponse(template.render(context, req))
 
