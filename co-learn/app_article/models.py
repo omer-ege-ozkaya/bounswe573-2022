158021c8c7a3828014882ec9951adf236c56df20
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-27 06:09:20 +0300

    Add created_at and modified_at fields to Article.

--- co-learn/app_article/models.py
+++ co-learn/app_article/models.py
@@ -9,7 +9,8 @@ class Article(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     like_count = models.IntegerField(default=0)
     learning_space = models.ForeignKey(LearningSpace, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-    
