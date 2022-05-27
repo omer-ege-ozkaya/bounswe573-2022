fa367bac4eb3380728964e0321aed2ae7f3f3132
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-27 03:40:09 +0300

    Create profile model.

--- co-learn/app_learning_space/models.py
+++ co-learn/app_learning_space/models.py
@@ -3,10 +3,9 @@ from django.db import models
 
 class LearningSpace(models.Model):
     name = models.CharField(max_length="64")
-    colearners = models.ManyToManyField("User")
+    colearners = models.ManyToManyField("Profile")
     description = models.TextField()
     thumbnail = models.ImageField()
 
     def __str__(self):
         return self.name
-    
