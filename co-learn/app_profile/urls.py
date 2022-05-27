afdbf4be9a09863e09579513949c6c4aa881ba1d
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-28 00:10:52 +0300

    Implement profile app.

--- co-learn/app_profile/urls.py
+++ co-learn/app_profile/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profile/", views.ProfilePageView.as_view(), name="profile_page_url")
+    path("profile/", views.profile_view, name="profile_page_url"),
+    path("profile/<int:profile_id>", views.profile_view, name="profile_page_url")
 ]
