ebc0b9f09aa89c3e83bef6b1d05108ffe4f4b887
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-27 03:51:26 +0300

    Create article model.

--- co-learn/app_article/models.py
+++ co-learn/app_article/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from app_profile.models import Profile
+from app_learning_space.models import LearningSpace
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(256)
+    body = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL)
+    like_count = models.IntegerField()
+    learning_space = models.ForeignKey(LearningSpace, on_delete=models.CASCADE)
--- co-learn/app_learning_space/models.py
+++ co-learn/app_learning_space/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from app_profile.models import Profile
 
 class LearningSpace(models.Model):
     name = models.CharField(max_length="64")
-    colearners = models.ManyToManyField("Profile")
+    colearners = models.ManyToManyField(Profile)
     description = models.TextField()
     thumbnail = models.ImageField()
 
