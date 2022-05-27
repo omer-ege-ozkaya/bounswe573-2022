87a03d5bb0cc56f80ea2302843a8c20f092bc4d8
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-27 04:00:30 +0300

    Bugfix article model.

--- co-learn/app_article/models.py
+++ co-learn/app_article/models.py
@@ -4,8 +4,8 @@ from app_learning_space.models import LearningSpace
 
 
 class Article(models.Model):
-    title = models.CharField(256)
+    title = models.CharField(max_length=256)
     body = models.TextField()
-    author = models.ForeignKey(Profile, on_delete=models.SET_NULL)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     like_count = models.IntegerField()
     learning_space = models.ForeignKey(LearningSpace, on_delete=models.CASCADE)
