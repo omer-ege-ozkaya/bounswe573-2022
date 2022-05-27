2114349f3224a20405ee47a5af98a1eb8ee1c7f5
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-27 19:02:37 +0300

    Add show/hide post form.

---
+++ co-learn/app_article/static/js/article.js
@@ -0,0 +1,8 @@
+function showHidePost() {
+    var postForm = document.getElementById("postForm")
+    if (postForm.style.display == "none") {
+        postForm.style.display = "block"
+    } else {
+        postForm.style.display = "none"
+    }
+}
