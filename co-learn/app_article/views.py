9e0d2dce8df5be83aa066a5246591a12670f71e7
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-27 08:11:26 +0300

    Bugfix post.

--- co-learn/app_article/views.py
+++ co-learn/app_article/views.py
@@ -8,7 +8,7 @@ from app_posts.models import Post
 def article_view(req, article_id):
     template = loader.get_template("app_article/article.html")
     article = Article.objects.get(id=article_id)
-    posts = Post.objects.filter()
+    posts = Post.objects.filter(article__id=article_id).order_by("created_at")
     context = {
         "article": article,
         "posts": posts
--- co-learn/app_posts/models.py
+++ co-learn/app_posts/models.py
@@ -8,6 +8,8 @@ class Post(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     like_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.body[:50]
