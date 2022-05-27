25c065896e55ee33aff28ae0cdb304c5b830a75a
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-27 07:52:30 +0300

    Create article view and template. Change post model.

--- co-learn/app_article/views.py
+++ co-learn/app_article/views.py
@@ -14,13 +14,3 @@ def article_view(req, article_id):
         "posts": posts
     }
     return HttpResponse(template.render(context, req))
-
-def learning_space_view(req, learning_space_id):
-    template = loader.get_template("app_learning_space/learning_space.html")
-    learning_space = LearningSpace.objects.get(id=learning_space_id)
-    articles = Article.objects.filter(id=learning_space_id).order_by("title")
-    context = {
-        "learning_space": learning_space,
-        "articles": articles
-    }
-    return HttpResponse(template.render(context, req))
