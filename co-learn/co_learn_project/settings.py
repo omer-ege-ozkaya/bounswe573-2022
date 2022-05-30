3ffe3fe3d6570ea9508041f759037308318eaa9c
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-05-31 02:37:01 +0300

    Postgres migration.

--- co-learn/co_learn_project/settings.py
+++ co-learn/co_learn_project/settings.py
@@ -82,9 +82,17 @@ WSGI_APPLICATION = 'co_learn_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
